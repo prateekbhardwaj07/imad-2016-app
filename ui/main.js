909a5270fa4609a02a3d4860318db18ef4103507
prateekbhardwaj07 <prateek.14bit1005@abes.ac.in>
2016-09-24 13:53:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,9 +3,11 @@ console.log('Loaded!');
 var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
-    marginLeft=marginLeft+5;
-    img.style.marginLeft=marginLeft+'px';
+    marginLeft = marginLeft + 5;
+    img.style.marginLeft = marginLeft +'px';
 }
 img.onclick = function(){
     var interval=(moveRight,50);
 }
+var elemnet=document.getElementById('text');
+element.innerHTML="Inner Html Feature";
