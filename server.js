6fc3882951a13f1d7f7af0e3ac8226acaaf2a79a
prateekbhardwaj07 <prateek.14bit1005@abes.ac.in>
2016-09-25 12:21:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ var articles = {
 };
 
 function  createTemplate(data){
-    //var title = data.title;
+   
     var heading = data.heading;
     var date = data.date;
     var content = data.content;
