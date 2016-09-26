4315baa839f3d449e054c2a97b0c5447ac254181
prateekbhardwaj07 <prateek.14bit1005@abes.ac.in>
2016-09-26 18:49:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,10 @@
 console.log('Loaded!');
 // moving the image rught
 var img=document.getElementById('madi');
-var marginLeft=0;
+var marginleft=0;
 function moveRight(){
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft + 'px';
+    marginleft = marginleft + 5;
+    img.style.marginLeft = marginleft + 'px';
 }
 img.onclick = function(){
     var interval = (moveRight , 50);
