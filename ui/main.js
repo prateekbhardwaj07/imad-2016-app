3a86028a44b0066ec5dfc0bca2641726bf3eb554
prateekbhardwaj07 <prateek.14bit1005@abes.ac.in>
2016-09-28 21:36:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,27 @@
 console.log('Loaded!');
-var element=document.getElementById('text/madi');
-element.innerHTML='Inner Html Feature';
+var button =document.getElementById('counter');
 
-// moving the image rught
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function(){
-    var interval = setInterval(moveRight , 50);
-    //img.style.marginLeft = '100px';
+button.onclick= function() {
+    
+    // create a request object
+    var request=new XMLHttpRequest();
+    
+    //Capturew the response and store it in variable
+    request.onreadystate= function(){
+        if(request.readystate === XMLHttpRequest.DONE)
+        {
+            //take some action
+            if(request.state === 200)
+            {
+                var counter=request.responseText;
+                var span= document.getElementById('count');
+                span.InnerHTML=counter.toString();
+            }
+        }
+        // Not done yet
+    };
+    
+    // Move the request
+    request.open('GET','http://prateekbhardwaj07/imad-2016-app/counter',true);
+    request.send(null);
 };
