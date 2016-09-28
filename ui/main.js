f769a533b6478e89333550baaa82f47058962446
prateekbhardwaj07 <prateek.14bit1005@abes.ac.in>
2016-09-28 16:08:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,6 +10,6 @@ function moveRight(){
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function(){
-    var interval = (moveRight , 50);
+    var interval = setInterval(moveRight , 50);
     //img.style.marginLeft = '100px';
 };
