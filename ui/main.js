c29917f773562f14b2347cfacdea7354d5920983
prateekbhardwaj07 <prateek.14bit1005@abes.ac.in>
2016-10-02 22:32:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,3 +26,19 @@ button.onclick = function() {
     request.open('GET','http://prateekbhardwaj07.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+var nameInput = document.getElementById('name');
+var name=nameInput.value;
+var submit = document.getElementById('sumbit_btn');
+submit.onclick = function () {
+    //Make request to server to send the names
+    
+    //capture a list of names and render it as list
+    var names = ['name1','name2','name3','name4'];
+    var list='';
+    
+    for(var i=0; i<names.length; i++)
+        list += '<li>' + names[i] + '</li>';
+        
+    var ul = document.getElementById('nameslist');
+    ul.innerHTML=list;
+}
