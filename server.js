e3d44dc3af929cbdbc94576fc5d42533bdac889e
prateekbhardwaj07 <prateek.14bit1005@abes.ac.in>
2016-11-08 21:04:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -134,7 +134,7 @@ res.send(JSON.stringify(names));
 app.get('/articles/:articleName',function(req,res){
     //articleName=article-one
     //aticles[aticleName]={} create content object for articles;
-    pool.query("SELECT * FROM article WHERE title = "+req.params.articleName , function(err,result){
+    pool.query("SELECT * FROM article WHERE title = '"+req.params.articleName+"'" , function(err,result){
         if(err)
         {
             res.status(500).send(err.toString);
