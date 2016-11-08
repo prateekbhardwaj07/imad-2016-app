5bd458697ee369d9e581ac0e3d61916883130816
prateekbhardwaj07 <prateek.14bit1005@abes.ac.in>
2016-11-08 21:09:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -137,7 +137,7 @@ app.get('/articles/:articleName',function(req,res){
     pool.query("SELECT * FROM article WHERE title = '"+req.params.articleName+"'" , function(err,result){
         if(err)
         {
-            res.status(500).send(err.toString);
+            res.status(500).send('Error in code');
         }
         else
         {
