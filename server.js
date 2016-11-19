38170e95779d7b3f4d22b4319a870290f7c247aa
prateekbhardwaj07 <prateek.14bit1005@abes.ac.in>
2016-11-20 00:07:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,6 +104,17 @@ app.get('/form',function (req, res) {
   res.send('get request to form.html');
 });
 
+function hash(input,salt){
+    // how to vreate a new hash
+    var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+}
+app.get('/hash/:input',function(req,res){
+    var hashedString =  hash(req.params.input,'this is some random string ');
+    res.send(hashedString);
+});
+
+
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
     //make a select  request
