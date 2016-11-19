668dfafc68d5e88b79aa7f63bedeee5c1d05790d
prateekbhardwaj07 <prateek.14bit1005@abes.ac.in>
2016-11-20 00:24:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/form',function (req, res) {
-  res.sendFile('form.html');
+  res.sendFile(path.join(__dirname, 'ui', 'form.html'));
 });
 
 function hash(input,salt){
