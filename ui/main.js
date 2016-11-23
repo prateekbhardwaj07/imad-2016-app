7fe1e3784261aef1637e8a5267dbf735640d92d0
prateekbhardwaj07 <prateek.14bit1005@abes.ac.in>
2016-11-24 00:49:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,12 +9,12 @@ $(document).ready(function() {
      var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
      var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
     if ($target) {
-                     var targetOffset = $target.offset().top;
-                     $(this).click(function() {
-                                $("#nav li a").removeClass("active");
-                                $(this).addClass('active');
-                     $('html, body').animate({scrollTop: targetOffset}, 1000);
-                     return false;
+                    var targetOffset = $target.offset().top;
+                    $(this).click(function() {
+                            $("#nav li a").removeClass("active");
+                            $(this).addClass('active');
+                    $('html, body').animate({scrollTop: targetOffset}, 1000);
+                    return false;
                 });
       }
       }
