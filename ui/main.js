8f74e499491310fc05116da84f31a1241e6951b4
prateekbhardwaj07 <prateek.14bit1005@abes.ac.in>
2016-11-24 00:47:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,11 +3,11 @@
 
 $(document).ready(function() {
  $('a[href*=#]').each(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-      && location.hostname == this.hostname
-      && this.hash.replace(/#/,'') ) {
-      var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
-      var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
+     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+     && location.hostname == this.hostname
+     && this.hash.replace(/#/,'') ) {
+     var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
+     var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
     if ($target) {
                      var targetOffset = $target.offset().top;
                      $(this).click(function() {
