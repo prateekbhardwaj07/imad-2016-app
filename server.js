2ccd1cb213542bdf311f1d6de388f692fa85bad2
prateekbhardwaj07 <prateek.14bit1005@abes.ac.in>
2016-11-24 10:24:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,12 +15,12 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
-/*app.use(bodyParser.json());*/
+/*app.use(bodyParser.json());
 app.use(session({
     secret: 'someRandomSecretValue',
     cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
 }));
-
+*/
 
 var articles = {
     'article-one': {
