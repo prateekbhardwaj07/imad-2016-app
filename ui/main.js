ed4a10712a50474046a1b31b6265e939fd76417e
prateekbhardwaj07 <prateek.14bit1005@abes.ac.in>
2016-11-24 09:19:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,20 +8,20 @@ $(document).ready(function() {
      && this.hash.replace(/#/,'') ) {
      var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
      var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
-    if ($target) {
-                            var targetOffset = $target.offset().top;
-                            $(this).click(function()
-                            {
-                            $("#nav li a").removeClass("active");
-                            $(this).addClass('active');
-                            $('html, body').animate({scrollTop: targetOffset}, 1000);
-                            return false;
-                            });
-     }
+    if ($target){
+                    var targetOffset = $target.offset().top;
+                    $(this).click(function()
+                    {
+                    $("#nav li a").removeClass("active");
+                    $(this).addClass('active');
+                    $('html, body').animate({scrollTop: targetOffset}, 1000);
+                    return false;
+                    });
+                }
      }
  });
 
-
+/*
 $(window).on("scroll",function(){
 	if(this.scrollTop()>= 150)
 	{	
@@ -34,7 +34,7 @@ $(window).on("scroll",function(){
 	$('#header').addClass('topheader');
 	}
 });
-
+*/
 document.getElementById('New User').onclick = function(){
 	location.href="http://prateekbhardwaj07.imad.hasura-app.io/form";
 };
