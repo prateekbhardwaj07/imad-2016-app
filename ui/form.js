849eebf5bcb42786ea0be1d89ff948588038d045
prateekbhardwaj07 <prateek.14bit1005@abes.ac.in>
2016-11-26 17:57:54 +0530

    [imad-console] Updates ui/form.js

--- ui/form.js
+++ ui/form.js
@@ -77,7 +77,7 @@ function prepareEventHandlers()
         var password = document.getElementById('password').value;
         console.log(username);
         console.log(password);
-        request.open('POST', '/form/create-user', true);
+        request.open('POST', '/create-user', true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({username: username, password: password}));  
         register.value = 'Registering...';
