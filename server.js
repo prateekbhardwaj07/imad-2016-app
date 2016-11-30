2c68c24bc5a23f1dd42e07f35eb0183de5a229c9
prateekbhardwaj07 <prateek.14bit1005@abes.ac.in>
2016-11-30 15:35:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -128,17 +128,13 @@ app.post('/create-user',function(req,res){
     var dbString=hash(password,salt);
     
     pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',[username,dbString], function(err,result){
-        if(!err)
+        if(err)
         {
             res.status(500).send(err.toString());
         }
-        else if(err)
-        {
-            res.send('User Sucessfully Created'+username);
-        }
         else
         {
-            
+            res.send('User Sucessfully Created'+username);
         }
     });
     
