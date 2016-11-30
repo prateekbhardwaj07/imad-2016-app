2fb17f480ea2ed7ceb30283aefd2162510a43b05
prateekbhardwaj07 <prateek.14bit1005@abes.ac.in>
2016-11-30 16:43:47 +0530

    [imad-console] Updates ui/form.js

--- ui/form.js
+++ ui/form.js
@@ -45,46 +45,44 @@ function prepareEventHandlers()
 				}
 				else
 				{
-				     return function createUser()
-				                    {
-                                		var register=document.getElementById('submitBtn');
-                                		register.onclick = function(){
-                                		 var request = new XMLHttpRequest();
-                                        
-                                        // Capture the response and store it in a variable
-                                        request.onreadystatechange = function ()
-                                		{
-                                          if (request.readyState === XMLHttpRequest.DONE)
-                                			  {
-                                              // Take some action
-                                              if (request.status === 200) {
-                                                  alert('User created successfully');
-                                                  register.value = 'Registered!';
-                                              } else {
-                                                  alert('Could not register the user');
-                                                  register.value = 'Register';
-                                              }
-                                          }
-                                        };
-                                        
-                                        // Make the request
-                                        var username = document.getElementById('username').value;
-                                        var password = document.getElementById('password').value;
-                                        console.log(username);
-                                        console.log(password);
-                                        request.open('POST', '/create-user', true);
-                                        request.setRequestHeader('Content-Type', 'application/json');
-                                        request.send(JSON.stringify({username: username, password: password}));  
-                                        register.value = 'Registering...';
-                                		};
-                                	}
-                                	
+				     return true;
 				}
 			}
 		}
  }
 	
-	
+	function createUser(){
+		var register=document.getElementById('submitBtn');
+		register.onclick = function(){
+		 var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function ()
+		{
+          if (request.readyState === XMLHttpRequest.DONE)
+			  {
+              // Take some action
+              if (request.status === 200) {
+                  alert('User created successfully');
+                  register.value = 'Registered!';
+              } else {
+                  alert('Could not register the user');
+                  register.value = 'Register';
+              }
+          }
+        }
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/create-user', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        register.value = 'Registering...';
+	}
+}
 //
 $(document).ready(function(){
     prepareEventHandlers();
