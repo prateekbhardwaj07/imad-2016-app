2907c077f4df9fe0f362e4e6d60330a4c460ace4
prateekbhardwaj07 <prateek.14bit1005@abes.ac.in>
2016-12-01 13:50:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
  $('a[href*=#]').each(function() {
      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
@@ -20,7 +21,44 @@ $(document).ready(function() {
 
 document.getElementById('New User').onclick = function(){
 	location.href="http://prateekbhardwaj07.imad.hasura-app.io/form";
-};
+}
+
+		var login =document.getElementById('login-btn');
+		login.onclick = function(){
+		 var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function ()
+		{
+          if (request.readyState === XMLHttpRequest.DONE)
+			  {
+              // Take some action
+              if (request.status === 200) {
+                  alert('User Logged In Sucessfully');
+              }
+              else if(request.status===403) {
+                  alert('Invalid Username/Password Value');
+              }
+              else 
+              {
+                  alert('Something Bad Occured On Server');
+              }
+          }
+        }
+        
+        // Make the request
+        var username = document.getElementById('uname').value;
+        var password = document.getElementById('Password').value;
+        request.open('POST', '/Login', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+	}
+
+
 });
 
-  
+	
+	
+
+
+
